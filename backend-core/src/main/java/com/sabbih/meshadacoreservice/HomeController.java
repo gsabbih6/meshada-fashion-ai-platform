e3afc7d879dd81452f7b6b3aa8a4e1c48b106ef7
gsabbih <gsabbih5@gmail.com>
2026-06-26 13:20:22 -0500

    Remove temporary debug endpoints from HomeController

--- backend-core/src/main/java/com/sabbih/meshadacoreservice/HomeController.java
+++ backend-core/src/main/java/com/sabbih/meshadacoreservice/HomeController.java
@@ -1,9 +1,5 @@
 package com.sabbih.meshadacoreservice;
 
-import com.sabbih.meshadacoreservice.ugc.UGCVideoRepository;
-import com.sabbih.meshadacoreservice.social.SocialCommentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +9,6 @@ import java.util.Map;
 @RestController
 public class HomeController {
 
-    @Value("${spring.datasource.url:not_set}")
-    private String dbUrl;
-
-    @Autowired
-    private UGCVideoRepository videoRepository;
-
-    @Autowired
-    private SocialCommentRepository commentRepository;
-
     @GetMapping("/")
     public ResponseEntity<Map<String, Object>> getHome() {
         return ResponseEntity.ok(Map.of(
@@ -30,32 +17,5 @@ public class HomeController {
                 "version", "1.0.0",
                 "message", "Meshada backend API entrypoint is fully functional."
         ));
-    }
-
-    @GetMapping("/debug-db")
-    public ResponseEntity<Map<String, Object>> debugDb() {
-        String maskedUrl = dbUrl;
-        if (dbUrl != null && dbUrl.contains("@")) {
-            int atIndex = dbUrl.indexOf("@");
-            int colonSlashSlash = dbUrl.indexOf("://");
-            if (colonSlashSlash != -1 && atIndex > colonSlashSlash) {
-                maskedUrl = dbUrl.substring(0, colonSlashSlash + 3) + "******" + dbUrl.substring(atIndex);
-            }
-        }
-        return ResponseEntity.ok(Map.of(
-                "dbUrl", maskedUrl,
-                "videoCount", videoRepository.count(),
-                "commentCount", commentRepository.count()
-        ));
-    }
-
-    @GetMapping("/debug-comments")
-    public ResponseEntity<java.util.List<com.sabbih.meshadacoreservice.social.SocialComment>> debugComments() {
-        return ResponseEntity.ok(commentRepository.findAll());
-    }
-
-    @GetMapping("/debug-videos")
-    public ResponseEntity<java.util.List<com.sabbih.meshadacoreservice.ugc.UGCVideo>> debugVideos() {
-        return ResponseEntity.ok(videoRepository.findAll());
     }
 }
